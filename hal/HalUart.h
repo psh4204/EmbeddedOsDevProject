fc640ed84ec5a27841df63361c1b26160ceec6f6
Park Si Hwan <psh4204@naver.com>
2023-08-30 19:57:43 +0900

    5.3 Receive input via UART

--- hal/HalUart.h
+++ hal/HalUart.h
@@ -3,5 +3,6 @@
 
 void Hal_uart_init(void);
 void Hal_uart_put_char(uint8_t ch);
+uint8_t Hal_uart_get_char(void);
 
 #endif /* HAL_HALUART_H_*/
--- hal/rvpb/Uart.c
+++ hal/rvpb/Uart.c
@@ -18,3 +18,20 @@ void Hal_uart_put_char(uint8_t ch)
     while(Uart->uartfr.bits.TXFF);
     Uart->uartdr.all = (ch & 0xFF);
 }
+
+uint8_t Hal_uart_get_char(void)
+{
+    uint8_t data;
+
+    while(Uart->uartfr.bits.RXFE);
+
+    data = Uart->uartdr.all; // Stack 쪽 메모리를 잘써라. ROM, HW레지스터 일일이 읽는거 보다 수십수백배 빠름.
+
+    if(data & 0xFFFFFFF00)
+    {
+        Uart->uartdr.all = 0xFF;
+        return 0;
+    }
+    
+    return (uint8_t)(data & 0xFF);
+}
